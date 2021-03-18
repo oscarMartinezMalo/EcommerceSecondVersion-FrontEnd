6775d443ce8b89a0122703bacf822d3c168b31eb
Oscar Home <ommalor@gmail.com>
2021-03-17 20:52:11 -0400

    UI Upload File

--- src/app/shared/components/file-update/file-update.component.ts
+++ src/app/shared/components/file-update/file-update.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, HostListener, Input, OnChanges,  } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, HostListener, Input, OnChanges, AfterViewChecked, OnDestroy,  } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 import { FileUrl } from './fileUrl.model';
 
@@ -7,38 +7,46 @@ import { FileUrl } from './fileUrl.model';
   templateUrl: './file-update.component.html',
   styleUrls: ['./file-update.component.scss']
 })
-export class FileUpdateComponent implements OnChanges {
+export class FileUpdateComponent implements OnChanges  {
   @Output() newItemEvent = new EventEmitter<FileUrl[]>();
   @Input() imageList: string[];
   @ViewChild('addImage') addImage: ElementRef;
   imageAddUrl = './assets/addProduct.png';
   fileList: FileUrl[] = [];
   @ViewChild('rowImages', { read: ElementRef }) rowImages: ElementRef;
-
+  scrollShowing = false;
   constructor() { }
 
   ngOnChanges(changes) {
     if (changes.imageList) {
-      changes.imageList.currentValue?.forEach(imgUrl =>{
+      changes.imageList.currentValue?.forEach(imgUrl => {
         this.fileList.push({file: null, imageUrl: imgUrl});
        });
     }
+
+    this.showPrevNextButtons();
+  }
+
+  showPrevNextButtons() {
+    // Wait until the View is Completed to show the previous and next button if scrollBar is visible
+    setTimeout(() => {
+      const ow = this.rowImages?.nativeElement.offsetWidth;
+      const sw = this.rowImages?.nativeElement.scrollWidth;
+      this.scrollShowing = (sw > ow) ? true : false;
+    }, 50);
   }
 
   async onFileSelected(event) {
-      const fileSelected = event.target.files[0];
-      const imageUrl = await this.parseFile(fileSelected) as string;
+    const fileSelected = event.target.files[0];
+    const imageUrl = await this.parseFile(fileSelected) as string;
 
-      this.fileList.push({file: fileSelected, imageUrl });
+    this.fileList.push({ file: fileSelected, imageUrl });
 
-      this.newItemEvent.emit(this.fileList);
+    this.newItemEvent.emit(this.fileList); // Emit Event to capture in the parent Component
 
-      this.addImage.nativeElement.value = ''; // Reset File Input to allow the submittion of the same file multiple times.
-  }
+    this.addImage.nativeElement.value = ''; // Reset File Input to allow the submittion of the same file multiple times.
 
-  onDeleteImage(index: number) {
-    this.fileList.splice(index, 1);
-    this.newItemEvent.emit(this.fileList);
+    this.showPrevNextButtons(); // Show Previous and Next buttons when the scrollbar is visible
   }
 
   parseFile(file: File) {
@@ -57,8 +65,11 @@ export class FileUpdateComponent implements OnChanges {
     });
   }
 
-  onImageBox( index: number ) {
-    (document.getElementById('image-' + index) as HTMLInputElement).click();
+  onDeleteImage(index: number) {
+    this.fileList.splice(index, 1);
+    this.newItemEvent.emit(this.fileList);
+
+    this.showPrevNextButtons(); // Show Previous and Next buttons when the scrollbar is visible
   }
 
   onNext() {
